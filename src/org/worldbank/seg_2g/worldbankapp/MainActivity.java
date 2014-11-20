4d15cbff157e5b5042c8eb0d57689da67a520667
Assaf <Assaf.Yossifoff@kcl.ac.uk>
2014-11-20 14:05:51 +0000

    working app

--- src/org/worldbank/seg_2g/worldbankapp/MainActivity.java
+++ src/org/worldbank/seg_2g/worldbankapp/MainActivity.java
@@ -1,5 +1,7 @@
 package org.worldbank.seg_2g.worldbankapp;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
@@ -9,19 +11,36 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.os.Build;
 
 public class MainActivity extends Activity {
 
+	private ArrayList<Country> countryList;
+	private QueryGenerator queryGen;
+	private Spinner countrySpinner;
+	private TextView testTextView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+		
+		// load all countries from local json file into list
+		countryList = new ArrayList<Country>();
+		queryGen = new QueryGenerator(this, testTextView);
+		queryGen.setCountryList(countryList);
+		
+		testTextView = (TextView) findViewById(R.id.textView1);
+		
+		// create country spinner
+		createSpinner();
+		
+		//TODO: Check if network is available
 	}
 
 	@Override
@@ -43,22 +62,32 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	private void createSpinner() {
+		countrySpinner = (Spinner) findViewById(R.id.country_spinner);
+		ArrayAdapter<Country> spinnerArrayAdapter = new ArrayAdapter<Country>(this, android.R.layout.simple_spinner_dropdown_item, countryList);
+		countrySpinner.setAdapter(spinnerArrayAdapter);
+		
+		// add selection listeners
+		countrySpinner.setOnItemSelectedListener(new OnItemSelectedListener() {
 
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view,
+					int pos, long id) {
+				if(!parent.getItemAtPosition(pos).toString().equalsIgnoreCase("aruba")) {
+					
+					// when a country is selected, create a query and start a graph activity
+					queryGen.createGraph((Country) parent.getItemAtPosition(pos), Settings.POPULATION, 1999, 2009, testTextView);
+				}
+			}
 
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 	}
 
 }
