3d58761962c8ec3ba0c53b4642e3994c0300479d
Assaf <Assaf.Yossifoff@kcl.ac.uk>
2014-11-22 10:20:01 +0000

    list now updates when text is typed in text field

--- src/org/worldbank/seg_2g/worldbankapp/GraphsActivity.java
+++ src/org/worldbank/seg_2g/worldbankapp/GraphsActivity.java
@@ -8,15 +8,16 @@ import org.json.JSONObject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.GraphView.GraphViewData;
@@ -28,12 +29,16 @@ public class GraphsActivity extends Activity {
 	private String queryJSON;
 	
 	private ArrayList<Country> countryList;
+	private ArrayList<Country> autoCompleteList;
 	private QueryGenerator queryGen;
-	private TextView countryTextView;
+	private EditText countryTextView;
 	
 	private GraphViewSeries populationEntries;
 	private LinearLayout graphLayout;
 	private ListView countryListView;
+	
+	private CountryListAdapter listAdapter;
+	private CountryListAdapter autoCompleteAdapter;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -76,24 +81,65 @@ public class GraphsActivity extends Activity {
 	private void createCountryViews() {
 		
 		// at the moment creates a text view and list view which overlap, need to fix the text view
-		countryTextView = (TextView) findViewById(R.id.countries_text_view);
+		countryTextView = (EditText) findViewById(R.id.countries_text_view);
 		countryListView = (ListView) findViewById(R.id.countries_list_view);
 		
-		CountryListAdapter listAdapter = new CountryListAdapter(this, countryList);
+		listAdapter = new CountryListAdapter(this, countryList);
 		countryListView.setAdapter(listAdapter);
 		
-		// add selection listeners
-		OnItemClickListener clickListener = new OnItemClickListener() {
+		// add list selection listener
+		countryListView.setOnItemClickListener(new OnItemClickListener() {
 			
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view, int pos,
 					long id) {
+				// when a country is selected from the list, get JSON data and create graph
 				queryJSON = queryGen.getJSON((Country) parent.getItemAtPosition(pos), Settings.POPULATION, 1999, 2009);
 				createGraph(queryJSON, parent.getItemAtPosition(pos).toString());	
 			}
-		};
+		});
 		
-		countryListView.setOnItemClickListener(clickListener);
+		// add EditText key listener, update list on key typed
+		countryTextView.addTextChangedListener(new TextWatcher() {
+
+			@Override
+			public void afterTextChanged(Editable e) {
+				// store current EditText input in lower case
+				String currentInput = countryTextView.getText().toString().toLowerCase();
+				
+				// if the text field is not empty
+				if (!currentInput.equals("")) {
+					
+					autoCompleteList = new ArrayList<Country>();
+					for (Country c: countryList) {
+						// for every country in the full country list, if it starts with the same text in the text field
+						// add it to a new country list
+						if(c.toString().toLowerCase().startsWith(currentInput)) {
+							autoCompleteList.add(c);
+						}
+					}
+					// create a new adapter using the new country list and set it to the ListView
+					autoCompleteAdapter = new CountryListAdapter(GraphsActivity.this, autoCompleteList);
+					countryListView.setAdapter(autoCompleteAdapter); 
+				}
+				// if the text field is empty, set the original adapter with the full country list to the ListView
+				else {
+					countryListView.setAdapter(listAdapter);
+				}
+			}
+
+			// not needed for this listener but needs to be implemented
+			@Override
+			public void beforeTextChanged(CharSequence arg0, int arg1,
+					int arg2, int arg3) {}
+			
+			// not needed for this listener but needs to be implemented
+			@Override
+			public void onTextChanged(CharSequence arg0, int arg1, int arg2,
+					int arg3) {}
+			
+		});
+		
 	}
 
 	protected void createGraph(String data, String countryName) {
