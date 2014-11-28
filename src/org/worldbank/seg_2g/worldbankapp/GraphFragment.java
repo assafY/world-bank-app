096d118ae926c141b7f5eeeb00ad3197ceda96f6
Assaf <Assaf.Yossifoff@kcl.ac.uk>
2014-11-28 02:28:41 +0000

    new graph implemented partially

--- src/org/worldbank/seg_2g/worldbankapp/GraphFragment.java
+++ src/org/worldbank/seg_2g/worldbankapp/GraphFragment.java
@@ -1,25 +1,29 @@
 package org.worldbank.seg_2g.worldbankapp;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.view.LineChartView;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.GraphView.GraphViewData;
-import com.jjoe64.graphview.GraphViewSeries;
-import com.jjoe64.graphview.LineGraphView;
-
 public class GraphFragment extends Fragment {
 
 	//TEMP class, once the action bar works properly it will be removed.
-		private GraphViewSeries populationEntries;
+		//private GraphViewSeries populationEntries;
 		private static RelativeLayout graphLayout;
 		
 		   // Inflate the layout
@@ -37,20 +41,27 @@ public class GraphFragment extends Fragment {
 		            
 		            // get total number of entries
 		            int totalEntries = titleValues.getInt("total");
-		            GraphViewData[] gvd = new GraphViewData[totalEntries];
+		            LineChartView graph = new LineChartView(context);
+		            List<PointValue> values = new ArrayList<PointValue>();
 		            
 		            JSONArray feedArray = dataFeed.getJSONArray(1);
 		            int jsonCounter = totalEntries - 1;
 		            
 		            for (int i = 0; i <totalEntries; ++i) {
 		                JSONObject json = feedArray.getJSONObject(jsonCounter--);
-		                gvd[i]= new GraphViewData(i, json.getInt("value"));
+		                values.add(new PointValue(i, json.getInt("value")));
 		            }
-		            populationEntries = new GraphViewSeries(gvd);
-		            GraphView graphView = new LineGraphView(context, countryName);
-		            graphView.addSeries(populationEntries);
+		            
+		            Line line = new Line(values).setColor(Color.BLUE).setCubic(true);
+		            List<Line> lines = new ArrayList<Line>();
+		            lines.add(line);
+		            
+		            LineChartData chartData = new LineChartData();
+		            chartData.setLines(lines);
+		            graph.setLineChartData(chartData);
+		            
 		            graphLayout.removeAllViews();
-		            graphLayout.addView(graphView);
+		            graphLayout.addView(graph);
 
 		        } catch (JSONException e) {
 		            // TODO: Handle exception
