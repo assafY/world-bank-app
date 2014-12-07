66737a4f4b14a7d842f85be154fba0dd35704461
andreiju <ajuganaru@outlook.com>
2014-12-07 20:18:24 +0000

    changed orientation to landscape only default year is 2010 internet connection check

--- src/org/worldbank/seg_2g/worldbankapp/MapActivity.java
+++ src/org/worldbank/seg_2g/worldbankapp/MapActivity.java
@@ -6,24 +6,27 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.NumberPicker;
+import android.widget.Toast;
 
 public class MapActivity extends Activity {
-	
+
+	public static final CharSequence NO_NETWORK_TEXT = "Your device has no network";
 	HTMLGenerator htmlGen;
 	WebView webMap;
-	int currentAttribute = 1, currentYear = 2014;
+	int currentAttribute = 1, currentYear = 2010;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
-		
+
 		onCreateExtra();
 	}
 
@@ -41,42 +44,72 @@ public class MapActivity extends Activity {
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_modify_attribute) {
-			new AttributePickerDialogFragment().show(getFragmentManager(), "options_attribute");
-		}
-		else if (id == R.id.action_modify_year) {
-			new YearPickerDialogFragment().show(getFragmentManager(), "options_year");
+			new AttributePickerDialogFragment().show(getFragmentManager(),
+					"options_attribute");
+		} else if (id == R.id.action_modify_year) {
+			new YearPickerDialogFragment().show(getFragmentManager(),
+					"options_year");
 		}
 		return super.onOptionsItemSelected(item);
 	}
 
-	
-	
+	// check if the device has network access
+	private boolean deviceHasNetwork() {
+
+		ConnectivityManager networkManager = null;
+		networkManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+		try {
+			boolean isDataConnected = networkManager.getNetworkInfo(
+					ConnectivityManager.TYPE_MOBILE).isConnectedOrConnecting();
+			boolean isWifiConnected = networkManager.getNetworkInfo(
+					ConnectivityManager.TYPE_WIFI).isConnectedOrConnecting();
+
+			if (isDataConnected || isWifiConnected) {
+				return true;
+			}
+		} catch (NullPointerException e) {
+			// null is returned on tablets, therefore return true
+			return true;
+		}
+
+		return false;
+
+	}
+
 	protected void onCreateExtra() {
-		
-		webMap = (WebView) findViewById(R.id.webView1);
-		WebSettings webMapSettings = webMap.getSettings();
-		webMapSettings.setJavaScriptEnabled(true);
-		webMapSettings.setBuiltInZoomControls(true);
-		webMapSettings.setDisplayZoomControls(false);
-		webMapSettings.setLoadWithOverviewMode(true);
-		webMapSettings.setUseWideViewPort(true);
-		htmlGen = new HTMLGenerator(this);
-		
-		webMap.loadData(htmlGen.getNoDataCode(), "text/html", null);
+
+		if (deviceHasNetwork()) {
+			webMap = (WebView) findViewById(R.id.webView1);
+			WebSettings webMapSettings = webMap.getSettings();
+			webMapSettings.setJavaScriptEnabled(true);
+			webMapSettings.setBuiltInZoomControls(true);
+			webMapSettings.setDisplayZoomControls(false);
+			webMapSettings.setLoadWithOverviewMode(true);
+			webMapSettings.setUseWideViewPort(true);
+			htmlGen = new HTMLGenerator(this);
+
+			webMap.loadData(htmlGen.getNoDataCode(), "text/html", null);
+		} else {
+			Toast.makeText(getApplicationContext(), NO_NETWORK_TEXT,
+					Toast.LENGTH_LONG).show();
+		}
 
 	}
 
 	private void drawMap() {
-		
-		this.setTitle(Settings.Attributes[currentAttribute] + " - " + currentYear);
+
+		this.setTitle(Settings.Attributes[currentAttribute] + " - "
+				+ currentYear);
 		webMap.loadData(htmlGen.getLoadingCode(), "text/html", null);
-		
+
 		new Thread(new Runnable() {
 
 			@Override
 			public void run() {
-				
-				final String s = htmlGen.getHTMLCode(currentAttribute, currentYear);
+
+				final String s = htmlGen.getHTMLCode(currentAttribute,
+						currentYear);
 				webMap.post(new Runnable() {
 
 					@Override
@@ -87,71 +120,73 @@ public class MapActivity extends Activity {
 			}
 		}).start();
 	}
-	
+
 	private class AttributePickerDialogFragment extends DialogFragment {
-				
+
 		private String[] getOptions() {
 			int nr = Settings.NUMBER_OF_ATTRIBUTES;
-			String [] options = new String[nr];
+			String[] options = new String[nr];
 			for (int i = 0; i < nr; i++) {
-				options[i] = Settings.Attributes[i+1];
+				options[i] = Settings.Attributes[i + 1];
 			}
 			return options;
 		}
-		
+
 		@Override
 		public Dialog onCreateDialog(Bundle savedInstanceState) {
 			AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-			builder.setItems(getOptions(), new DialogInterface.OnClickListener() {
-				@Override
-				public void onClick(DialogInterface dialog, int which) {
-					currentAttribute = which+1;
-					drawMap();
-				}
-			});
+			builder.setItems(getOptions(),
+					new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							if (deviceHasNetwork()) {
+								currentAttribute = which + 1;
+								drawMap();
+							} else {
+								Toast.makeText(getApplicationContext(),
+										NO_NETWORK_TEXT, Toast.LENGTH_LONG)
+										.show();
+							}
+						}
+					});
 			return builder.create();
 		}
 	}
-	
+
 	private class YearPickerDialogFragment extends DialogFragment {
-		
+
 		private String[] getOptions() {
-			String [] options = new String[54];
+			String[] options = new String[54];
 			for (int i = 0; i < 54; i++) {
-				options[i] = ""+(i+1961);
+				options[i] = "" + (i + 1961);
 			}
 			return options;
 		}
-		
-//		@Override
-//		public Dialog onCreateDialog(Bundle savedInstanceState) {
-//			AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-//			builder.setItems(getOptions(), new DialogInterface.OnClickListener() {
-//				@Override
-//				public void onClick(DialogInterface dialog, int which) {
-//					currentYear = which+1961;
-//					drawMap();
-//				}
-//			});
-//			return builder.create();
-//		}
-		
+
 		@Override
 		public Dialog onCreateDialog(Bundle savedInstanceState) {
 			AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-			final NumberPicker picker = new NumberPicker(getApplicationContext());
+			final NumberPicker picker = new NumberPicker(
+					getApplicationContext());
 			picker.setMinValue(1961);
 			picker.setMaxValue(2014);
 			picker.setValue(currentYear);
 			picker.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
 			builder.setView(picker);
-			builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-				@Override
-				public void onClick(DialogInterface dialog, int id) {
-					currentYear = picker.getValue();
-					drawMap();
-				}
-			});
+			builder.setPositiveButton("OK",
+					new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int id) {
+							if (deviceHasNetwork()) {
+								currentYear = picker.getValue();
+								drawMap();
+							} else {
+								Toast.makeText(getApplicationContext(),
+										NO_NETWORK_TEXT, Toast.LENGTH_LONG)
+										.show();
+							}
+						}
+					});
 			return builder.create();
 		}
 	}
