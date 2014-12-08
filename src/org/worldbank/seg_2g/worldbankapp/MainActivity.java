a9bc649a2633f8c9669016bed9326537b6e95e95
Latif <mohammedlatif@bbls-MacBook-Pro.local>
2014-12-08 17:03:18 +0000

    final homescreen

--- src/org/worldbank/seg_2g/worldbankapp/MainActivity.java
+++ src/org/worldbank/seg_2g/worldbankapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 
 public class MainActivity extends Activity {
 
