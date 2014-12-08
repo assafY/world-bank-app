df3a4458b8e0a15d42657200e4a4e79c2b46242a
Assaf <Assaf.Yossifoff@kcl.ac.uk>
2014-12-08 13:12:42 +0000

    line removed from graph adapter

--- src/org/worldbank/seg_2g/worldbankapp/GraphAdapter.java
+++ src/org/worldbank/seg_2g/worldbankapp/GraphAdapter.java
@@ -23,7 +23,7 @@ public class GraphAdapter extends FragmentPagerAdapter {
 
 	@Override
 	public Fragment getItem(int position) {
-		pagePosition = position + 1;
+		//pagePosition = position + 1;
 		return GraphFrameHolder.newInstance(position + 1);
 	}
 	
